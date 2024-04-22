407faae50fab64f617ed77a83812e9564d4da264
Fatma Birel <Fatma Birel@DESKTOP-Q270QVE>
2024-04-22 17:34:22 +0300

    Web API Layer

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Concrete;
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
 
 internal class Program
 {
@@ -22,20 +23,28 @@ internal class Program
     private static void ProductTest()
     {
         ProductManager productManager = new ProductManager(new EfProductDal());
+
+        //Product product = new Product { ProductName = "D", CategoryID = 1, UnitPrice = 140, UnitsInStock = 3 };
+        //productManager.Add(product);
+
+        foreach (var category in productManager.GetAll().Data)
+        {
+            Console.WriteLine(category.ProductName);
+        }
         var result = productManager.GetProductDetails();
 
-        if(result.Success == true)
-        {
-            foreach (var product in productManager.GetProductDetails().Data)
-            {
-                Console.WriteLine(product.ProductName + "-" + product.CategoryName);
-            }
-        }
-        else
-        {
-            Console.WriteLine(result.Message);
-        }
+        //if(result.Success == true)
+        //{
+        //    foreach (var product in productManager.GetProductDetails().Data)
+        //    {
+        //        Console.WriteLine(product.ProductName + "-" + product.CategoryName);
+        //    }
+        //}
+        //else
+        //{
+        //    Console.WriteLine(result.Message);
+        //}
 
-        
+
     }
 }
